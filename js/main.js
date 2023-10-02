2dabfa1da2b098b7a21216b7abf02798d49ea842
tsukiji <130459247+tsukijidsgn@users.noreply.github.com>
2023-10-02 15:38:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -116,8 +116,8 @@
 	};
 
 	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
+		if ($('#skills').length > 0 ) {
+			$('#skills').waypoint( function( direction ) {
 										
 				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
 					setTimeout( pieChart , 400);					
@@ -131,7 +131,7 @@
 
 	// Loading page
 	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
+		$(".loader").fadeOut("slow");
 	};
 
 	
